f031ad32eb64e6eb103b4d185886d5438bf45f80
jamesley joseph <jamesleyjoseph@jamesleys-Mac-mini.local>
2021-09-13 19:55:54 -0400

    add boilerplate logic for Client

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,7 +1,27 @@
+import java.net.Socket;
+
+import java.io.DataInputStream;
 
 public class Client {
+
+	private static Socket socket;
+
  
-	public static void main(String []args) {
-		System.out.println("Hello World Client");
+	public static void main(String []args) throws Exception {
+		
+		
+		String serverAdress = "127.0.0.1";
+		int port = 5000; 
+		socket = new Socket(serverAdress, port); 
+		
+		System.out.format("The server is running on %s:%d%n",serverAdress , port); 
+		
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		
+		String helloMessageFromServer = in.readUTF()
+; 
+		System.out.println(helloMessageFromServer);
+		
+		socket.close();
 	}
 }
