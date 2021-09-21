3bfbd06b6d7257d9370be528de083423c359f2ee
jamesley joseph <jamesleyjoseph@jamesleys-Mac-mini.local>
2021-09-20 20:17:59 -0400

    blueprint for client

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,6 +1,12 @@
+
+import java.io.DataOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 
+
 import java.io.DataInputStream;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
 
@@ -11,17 +17,104 @@ public class Client {
 		
 		
 		String serverAdress = "127.0.0.1";
-		int port = 5000; 
+		int port = 5002;
+
+
+		Scanner inputScanner = new Scanner(System.in);
+
+		// Input et validation de l'adresse IP
+		System.out.println(" Enter the server's IP address:");
+
+		serverAdress = inputScanner.nextLine();
+		while (!Client.validIPAddress(serverAdress)) {
+			System.out.println(" Wrong IP Address. Try again");
+			serverAdress = System.console().readLine();
+		}
+
+		System.out.println("Enter the server's port:");
+		port = inputScanner.nextInt();
+		while (!Client.validatePort(port)) {
+			System.out.println("Invalid port. Try again.");
+			port = Integer.parseInt(System.console().readLine());
+		}
+
 		socket = new Socket(serverAdress, port); 
 		
 		System.out.format("The server is running on %s:%d%n",serverAdress , port); 
 		
 		DataInputStream in = new DataInputStream(socket.getInputStream());
-		
-		String helloMessageFromServer = in.readUTF()
-; 
-		System.out.println(helloMessageFromServer);
-		
-		socket.close();
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		System.out.println(in.readUTF());
+
+		while (true) {
+			String command = inputScanner.nextLine();
+
+			if (command == "") continue;
+			out.writeUTF(command);
+			commandCenter(command, in);
+
+			TimeUnit.MILLISECONDS.sleep(100);
+
+			while(in.available() != 0)
+			{
+				String serverResponse = in.readUTF();
+				if (serverResponse.isEmpty()) break;
+				System.out.println(serverResponse);
+			}
+		}
+	}
+
+	public static void commandCenter(String command, DataInputStream in) {
+		String[] consoleInput = command.split(" ");
+
+		if(consoleInput.length != 0) {
+			switch(consoleInput[0]) {
+				case "cd":
+					// Todo cd function
+					// cd(consoleInput[1]);
+					break;
+				case "ls":
+					// Todo ls function
+					// ls();
+					break;
+				case "mkdir":
+					// Todo mkdir function
+					// mkdir(consoleInput[1]);
+					break;
+				case "delete":
+					// Todo delete function
+					// delete(consoleInput[1]);
+					break;
+				case "upload":
+					// Todo upload function
+					// upload(consoleInput[1]);
+					break;
+				case "download":
+					Boolean zip = false;
+					if(consoleInput[2] =="-Z"){
+						zip = true;
+					}
+					// Todo Download function
+					// download(consoleInput[1], zip);
+					break;
+				default:
+					break;
+			}
+		} else return;
+
+	}
+
+	public static boolean validatePort(final int port) {
+		return port >= 5002 && port <= 5049;
+	}
+	public static Boolean validIPAddress(String IP) {
+		try {
+			if (InetAddress.getByName(IP) instanceof InetAddress) {
+				return true;
+			};
+		} catch(Exception e) {
+			return false;
+		}
+		return false;
 	}
 }
