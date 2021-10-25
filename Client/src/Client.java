41addd7e475da6e0c77f042463d469f135cabb05
jamesley joseph <jamesleyjoseph@jamesleys-Mac-mini.local>
2021-10-25 19:46:26 -0400

    add headers

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,8 +1,10 @@
-
-import javafx.scene.SubScene;
-import sun.lwawt.macosx.CSystemTray;
-
-import javax.xml.crypto.Data;
+/**
+ * Auteurs : Jamesley Joseph (1990552) , Manel Ben jemaa (1871842)  , Alex Hua (1994253)
+ * Description: Serveur qui receveras des commandes du client
+ *             dans le but de stocker des donnees ou de naviger
+ *             dans un repertoire
+ *  Date : 25 Octobre 2021
+ **/
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -14,7 +16,7 @@ import java.util.zip.ZipOutputStream;
 public class Client {
 
 	private static Socket socket;
-	private static String currentRep;
+	private static String currentRep = System.getProperty("user.dir");;
 
 
 	public static void main(String []args) throws Exception {
@@ -26,25 +28,24 @@ public class Client {
 
 		Scanner inputScanner = new Scanner(System.in);
 
-		// Input et validation de l'adresse IP
-		System.out.println(" Enter the server's IP address:");
+		System.out.println("Entrer l'adresse IP du serveur:");
 
 		serverAdress = inputScanner.nextLine();
 		while (!Client.validIPAddress(serverAdress)) {
-			System.out.println(" Wrong IP Address. Try again");
+			System.out.println("Adresse IP Invalide. Essayer de nouveau");
 			serverAdress = System.console().readLine();
 		}
 
-		System.out.println("Enter the server's port:");
+		System.out.println("Veuilliez entrer le port du serveur:");
 		port = inputScanner.nextInt();
 		while (!Client.validatePort(port)) {
-			System.out.println("Invalid port. Try again.");
+			System.out.println("port Invalide.Essayer de nouveau");
 			port = Integer.parseInt(System.console().readLine());
 		}
 
 		socket = new Socket(serverAdress, port);
 
-		System.out.format("The server is running on %s:%d%n",serverAdress , port);
+		System.out.format("Connection sur %s:%d%n",serverAdress , port);
 
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
@@ -54,7 +55,7 @@ public class Client {
 			String command = inputScanner.nextLine();
 
 			if (command.isEmpty()) continue;
-			out.writeUTF(command); // Envoie vers le serveur
+			out.writeUTF(command);
 			commandCenter(command, in, out);
 
 			TimeUnit.MILLISECONDS.sleep(100);
@@ -68,18 +69,22 @@ public class Client {
 		}
 	}
 
+
+	/**
+	 * Fonction qui dicte quelle fonction appeler en fonction de input du client
+	 * \param [DataInputStream: le input stream vers lequelle le client recevra les donnees du serveur ,
+	 *        DataOutputStream: le output stream vers lequelle le serveur recevra les donnees du client]
+	 * \return void
+	 */
 	public static void commandCenter(String command, DataInputStream in , DataOutputStream out) throws  Exception {
 		String[] consoleInput = command.split(" ");
 
 		if(consoleInput.length != 0) {
 			switch(consoleInput[0]) {
 				case "upload":
-					// Todo Download function
 					upload(consoleInput[1] , out);
 					break;
 				case "download":
-
-					// Todo Download function
 					boolean isArchived = false;
 					if(consoleInput.length == 3){
 						isArchived = true;
@@ -97,9 +102,20 @@ public class Client {
 
 	}
 
+	/**
+	 * Determiner si le port passer en parametre est valide
+	 * \param [int: port]
+	 * \return un boolean qui definie sur le port est valide ou non
+	 */
 	public static boolean validatePort(final int port) {
 		return port >= 5002 && port <= 5049;
 	}
+
+	/**
+	 * Determiner si l'adresse IP passer en parametre est valide
+	 * \param [String: IP adress]
+	 * \return un boolean qui definie sur l'adress IP est valide ou non
+	 */
 	public static Boolean validIPAddress(String IP) {
 		try {
 			if (InetAddress.getByName(IP) instanceof InetAddress) {
@@ -111,9 +127,14 @@ public class Client {
 		return false;
 	}
 
+	/**
+	 * \Description fonction qui permet de televerser un fichier du client vers le serveur de stockage
+	 * \param String: le nom du nouveau du fichier a televerser
+	 * \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+	 * \return void
+	 */
 	public static void upload(String nomFichier, DataOutputStream out ) throws Exception{
 
-		String currentRep = System.getProperty("user.dir");
 		File  newFile = new File(currentRep + "/" + nomFichier);
 
 		InputStream uploadedFile = new FileInputStream(newFile);
@@ -127,34 +148,41 @@ public class Client {
 			out.write(buffer, 0, packetSize);
 
 		}
-		uploadedFile.close();
+		out.flush();
 	}
 
+	/**
+	 * \Description fonction qui permet de telecharger un fichier du serveur de stockage vers le client
+	 * \param String: le nom du nouveau du fichier a televerser
+	 * \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+	 * \return void
+	 */
 	public static void download(String nomFichier, boolean isArchived, DataInputStream in) throws Exception{
 
-		String currentRep = System.getProperty("user.dir");
 		DataInputStream fin =  new DataInputStream(socket.getInputStream());
 
 		if(isArchived){
 			nomFichier = nomFichier.substring(0, nomFichier.indexOf(".")) + ".zip";
 		}
-		System.out.println("nom Fichier " + nomFichier);
+
 		File  newFile = new File(currentRep + "/" + nomFichier);
 		FileOutputStream fileOut = new FileOutputStream(newFile);
 
 		if(isArchived) {
 			FileOutputStream zipFile = new FileOutputStream(currentRep + "/" + nomFichier);
 			ZipOutputStream zipFileOutput = new ZipOutputStream(zipFile);
-			zipFileOutput.putNextEntry(new ZipEntry(newFile.getName())); // copies les byte de ran.jpeg dans le fichier zip
+			zipFileOutput.putNextEntry(new ZipEntry(newFile.getName()));
 		}
+
 		int size = in.readInt();
 		byte[] buffer = new byte[size];
 		int sizeOfPacket;
-		while ((sizeOfPacket = fin.read(buffer)) != -1) {   // store packet de donne dans le fichier
+
+		while ((sizeOfPacket = fin.read(buffer)) != -1) {
 			if(size <= 0)
 				break;
 			size -= sizeOfPacket;
-			fileOut.write(buffer, 0, sizeOfPacket);   // store les donne contenue dans le buffer et le met dans le fichier "newFile" cree
+			fileOut.write(buffer, 0, sizeOfPacket);
 		}
 		fileOut.flush();
 		fileOut.close();
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,5 +1,10 @@
-import com.sun.tools.internal.ws.wsdl.document.Output;
-
+/**
+* Auteurs : Jamesley Joseph (1990552) , Manel Ben jemaa (1871842)  , Alex Hua (1994253)
+* Description: Serveur qui receveras des commandes du client
+*             dans le but de stocker des donnees ou de naviger
+*             dans un repertoire
+*  Date : 25 Octobre 2021
+**/
 import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -16,7 +21,6 @@ public class Server {
 
     private static ServerSocket listener;
 
-
     public static void main(String[] args) throws Exception {
 
         int clientNumber = 0;
@@ -27,19 +31,19 @@ public class Server {
         Scanner inputScanner = new Scanner(System.in);
 
         // Input et validation de l'adresse IP
-        System.out.println(" Enter the server's IP address:");
+        System.out.println("Entrer l'adresse IP du serveur:");
         System.out.println(System.getProperty("user.dir"));
 
         serverAdress = inputScanner.nextLine();
         while (!Server.validIPAddress(serverAdress)) {
-            System.out.println(" Wrong IP Address. Try again");
+            System.out.println("Adresse IP Invalide. Essayer de nouveau");
             serverAdress = System.console().readLine();
         }
 
-        System.out.println("Enter the server's port:");
+        System.out.println("Veuilliez entrer le port du serveur:");
         serverPort = inputScanner.nextInt();
         while (!Server.validatePort(serverPort)) {
-            System.out.println("Invalid port. Try again.");
+            System.out.println("port Invalide.Essayer de nouveau");
             serverPort = Integer.parseInt(System.console().readLine());
         }
 
@@ -50,7 +54,7 @@ public class Server {
 
         listener.bind(new InetSocketAddress(serverIP, serverPort));
 
-        System.out.format("The server is running on %s:%d%n", serverAdress, serverPort);
+        System.out.format("Connection sur %s:%d%n", serverAdress, serverPort);
 
         try {
             while (true) {
@@ -62,10 +66,21 @@ public class Server {
 
     }
 
+
+    /**
+     * Determiner si le port passer en parametre est valide
+     * \param int: port
+     * \return un boolean qui definie sur le port est valide ou non
+     */
     public static boolean validatePort(final int port) {
         return port >= 5002 && port <= 5049;
     }
 
+    /**
+     * Determiner si l'adresse IP passer en parametre est valide
+     * \param String: IP adress
+     * \return un boolean qui definie sur l'adress IP est valide ou non
+     */
     public static Boolean validIPAddress(String IP) {
         try {
             if (InetAddress.getByName(IP) instanceof InetAddress) {
@@ -93,12 +108,17 @@ class ClientHandler extends Thread {
         this.clientNumber = clientNumber;
         this.serverAdress = serverAdress;
         this.serverPort = serverPort;
-        System.out.println("New connection with client#" + clientNumber + " at " + socket);
+        System.out.println("Nouveau connection avec le client#" + clientNumber + " a " + socket);
 
     }
 
+    /**
+     * Fonction qui dicte quelle fonction appeler en fonction de input recu du client
+     * \param DataInputStream: le input stream vers lequelle le client envoyera des donnees vers le serveur
+     * \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+     * \return void
+     */
     public void commandCenter(DataInputStream in, DataOutputStream out) throws Exception {
-        // Date et temps pour l'affichage
         LocalDate date = java.time.LocalDate.now();
         int hours = java.time.LocalTime.now().getHour(), minutes = java.time.LocalTime.now().getMinute(),
                 seconds = java.time.LocalTime.now().getSecond();
@@ -132,7 +152,7 @@ class ClientHandler extends Thread {
 
                     break;
                 case "upload":
-                    upload(clientInputs[1], in);
+                    upload(clientInputs[1], in, out);
                     break;
                 case "download":
                     if (clientInputs.length == 3 && clientInputs[2].contains("-z")) {
@@ -143,7 +163,9 @@ class ClientHandler extends Thread {
                         out.writeUTF("le format de fichier que vous voulez n'existe pas \n");
                    }else{
                         download(clientInputs[1], out);
+
                     }
+
                     break;
                 default:
                     out.writeUTF("Command not found, please try again.\n");
@@ -156,28 +178,39 @@ class ClientHandler extends Thread {
 
     }
 
+
+    /**
+     * \Description Fonction qui permet de cree un socket du serveur vers le client et donne des updates lorsqu'un erreur se produit.
+     * \return void
+     */
     public void run() {
         try {
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            out.writeUTF("Hello from server- you are client#" + clientNumber);
+            out.writeUTF("Bonjour du serveur- vous etes le client#" + clientNumber);
             commandCenter(in, out);
             clientNumber++;
 
         } catch (Exception e) {
-            System.out.println("Error handling client#" + clientNumber + " : " + e);
+            System.out.println("Client#"+ clientNumber + " a ete deconnecter");
 
         } finally {
             try {
                 socket.close();
             } catch (IOException e) {
-                System.out.println("Couldn't close a socket, what's going on?");
+                System.out.println("Impossibilite de fermer le socket!");
 
             }
-            System.out.println("Connection with client#" + clientNumber + "closed");
+            System.out.println("Connection avec le client#" + clientNumber + "fermer");
+            clientNumber -= 1;
         }
     }
-
+    /**
+     *  \Description Fonction qui permet se deplacer dans un repertoire enfant d'un repertoir du serveur de stockage
+     *  \param String: le nom du serveur auquelle ont veut se deplacer
+     *  \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+     * \return void
+     */
     public void cd(String name, DataOutputStream out) throws Exception {
         if (name.startsWith("..")) {
             String[] folderBackward = name.split("/");
@@ -190,6 +223,8 @@ class ClientHandler extends Thread {
             }
 
             currentRep = newPath;
+            int lenght = newPath.split("/").length;
+            name = newPath.split("/")[lenght -1];
         } else if (ls(out, true).contains(name)) {
 
             currentRep += "/" + name;
@@ -197,9 +232,17 @@ class ClientHandler extends Thread {
             out.writeUTF(name + "ce dossier n'est pas present dans le Dossier parent");
             return;
         }
-        out.writeUTF("Vous êtes dans le dossier " + name);
+        out.writeUTF("Vous etes dans le dossier " + name);
     }
 
+    /**
+     *  \Description Fonction qui permet de chercher ou d'afficher tous les fichiers contenu dans le repertoir
+     *               que le serveur de stockage se trouve actuellement
+     *  \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+     *  \param boolean : un parametre qui sert a savoir si on veut affichier le contenue au niveau du client
+     *         ou si on veut faire un simple recherche au niveau du serveur. mis a False par default
+     * \return List<String> : la liste des fichier se trouvant dans ce repertoire
+     */
     public List<String> ls(DataOutputStream out, boolean isLookout) throws Exception {
         File newRep = new File(currentRep);
         File[] list = newRep.listFiles();
@@ -207,8 +250,7 @@ class ClientHandler extends Thread {
 
         for (int i = 0; i < list.length; i++) {
             if (isLookout) {
-                if (list[i].isDirectory())
-                    lsList.add(list[i].getName());
+                lsList.add(list[i].getName());
             } else {
                 if (list[i].isDirectory()) {
                     out.writeUTF("[Folder] " + list[i].getName());
@@ -222,6 +264,13 @@ class ClientHandler extends Thread {
         return lsList;
     }
 
+    /**
+     *  \Description fonction qui permet de cree un nouveau repertoir dans le repertoir parents
+     *              que le serveur de stockage se trouve actuellement
+     *  \param String: le nom du nouveau repertoire qu'on veut cree
+     *  \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+     * \return void
+     */
     public void mkdir(String name, DataOutputStream out) throws Exception {
 
         File newFile = new File(currentRep + "/" + name);
@@ -231,6 +280,13 @@ class ClientHandler extends Thread {
             out.writeUTF("Error: aucun dossier n'as ete cree. Essayer encore ");
         }
     }
+    /**
+     *  \Description fonction qui permet de supprimer un fichier ou un repertoire existant dans le repertoire parents
+     *              que le serveur de stockage se trouve actuellement
+     *  \param String: le nom du nouveau du fichier/repertoire qu'on veut supprimer
+     *  \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+     * \return void
+     */
 
     public void delete(String name, DataOutputStream out) throws Exception {
 
@@ -240,11 +296,19 @@ class ClientHandler extends Thread {
         }
         File newRep = new File(currentRep + "/" + name);
         newRep.delete();
-        out.writeUTF("Le dossier " + name + " a été supprime.");
+        out.writeUTF( name + " a ete supprime.");
     }
 
-    public void upload(String nomFichier, DataInputStream in) throws Exception {
+    /**
+     * \Description fonction qui permet de televerser un fichier du client vers le serveur de stockage
+     * \param String: le nom du nouveau du fichier a televerser
+     * \param DataInputStream: le input stream vers lequelle le client envoyera des donnees vers le serveur
+     * \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+     * \return void
+     */
 
+    public void upload(String nomFichier, DataInputStream in, DataOutputStream out) throws Exception {
+        DataInputStream fin =  new DataInputStream(socket.getInputStream());
 
         System.out.println(currentRep + "/" + nomFichier);
 
@@ -253,17 +317,29 @@ class ClientHandler extends Thread {
         int size = in.readInt();
         byte[] buffer = new byte[size];
         int sizeOfPacket;
-        DataInputStream fin =  new DataInputStream(socket.getInputStream());
         while ((sizeOfPacket = fin.read(buffer)) != -1) {
             if(size <= 0)
                 break;
             size -= sizeOfPacket;
-            fileOut.write(buffer, 0, sizeOfPacket); // store les donne contenue dans le buffer et le met dans le fichier "newFile" cree
+            fileOut.write(buffer, 0, sizeOfPacket);
         }
+        fileOut.flush();
         fileOut.close();
+        out.writeUTF("Le fichier " + nomFichier + " a bien ete televerser.");
     }
 
+    /**
+     * \Description fonction qui permet de telecharger un fichier du serveur de stockage vers le client
+     * \param String: le nom du nouveau du fichier a televerser
+     * \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+     * \return void
+     */
     public void download(String nomFichier, DataOutputStream out) throws Exception {
+
+        if (!ls(out, true).contains(nomFichier)) {
+            out.writeUTF("Aucun dossier ou fichier de ce nom existe");
+            return;
+        }
 
         File newFile = new File(currentRep + "/" + nomFichier);
         InputStream uploadedFile = new FileInputStream(newFile);
@@ -276,12 +352,16 @@ class ClientHandler extends Thread {
 
         while ((packetSize = uploadedFile.read(buffer)) != -1) {
             out.write(buffer, 0, packetSize);
-            System.out.println("size " + packetSize);
         }
-        out.writeInt(-1);
-        uploadedFile.close();
+        out.flush();
     }
 
+    /**
+     * \Description fonction qui permet de cree un fichier zip a partir d'une fichier donnee
+     * \param String: le nom du nouveau du fichier a zipper
+     * \param DataOutputStream: le output stream vers lequelle le serveur envoyeras des donnees vers le client
+     * \return void
+     */
     public void Zip(String nomFichier, DataOutputStream out) throws Exception {
 
         File newFile = new File(currentRep + "/" + nomFichier);
@@ -290,7 +370,7 @@ class ClientHandler extends Thread {
 
         FileOutputStream zipFile = new FileOutputStream(currentRep + "/" + zipFileName);
         ZipOutputStream zipFileOutput = new ZipOutputStream(zipFile);
-        zipFileOutput.putNextEntry(new ZipEntry(newFile.getName())); // copies les byte de ran.jpeg dans le fichier zip
+        zipFileOutput.putNextEntry(new ZipEntry(newFile.getName()));
 
         InputStream fileToZip = new FileInputStream(newFile);
 
@@ -306,7 +386,6 @@ class ClientHandler extends Thread {
         zipFileOutput.close();
         fileToZip.close();
         download(zipFileName, out);
-        delete(zipFileName, out);
     }
 
 }
