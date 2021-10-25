7c9698934ac3f85ccad80a13baa483a41855e761
jamesley joseph <jamesleyjoseph@jamesleys-Mac-mini.local>
2021-10-24 21:00:57 -0400

    finish download function

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,21 +1,24 @@
 
-import java.io.DataOutputStream;
+import javafx.scene.SubScene;
+
+import javax.xml.crypto.Data;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-
-
-import java.io.DataInputStream;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Client {
 
 	private static Socket socket;
+	private static String currentRep;
 
- 
+
 	public static void main(String []args) throws Exception {
-		
-		
+
+
 		String serverAdress = "127.0.0.1";
 		int port = 5002;
 
@@ -38,10 +41,10 @@ public class Client {
 			port = Integer.parseInt(System.console().readLine());
 		}
 
-		socket = new Socket(serverAdress, port); 
-		
-		System.out.format("The server is running on %s:%d%n",serverAdress , port); 
-		
+		socket = new Socket(serverAdress, port);
+
+		System.out.format("The server is running on %s:%d%n",serverAdress , port);
+
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 		System.out.println(in.readUTF());
@@ -49,9 +52,9 @@ public class Client {
 		while (true) {
 			String command = inputScanner.nextLine();
 
-			if (command == "") continue;
-			out.writeUTF(command);
-			commandCenter(command, in);
+			if (command.isEmpty()) continue;
+			out.writeUTF(command); // Envoie vers le serveur
+			commandCenter(command, in, out);
 
 			TimeUnit.MILLISECONDS.sleep(100);
 
@@ -64,38 +67,26 @@ public class Client {
 		}
 	}
 
-	public static void commandCenter(String command, DataInputStream in) {
+	public static void commandCenter(String command, DataInputStream in , DataOutputStream out) throws  Exception {
 		String[] consoleInput = command.split(" ");
 
 		if(consoleInput.length != 0) {
 			switch(consoleInput[0]) {
-				case "cd":
-					// Todo cd function
-					// cd(consoleInput[1]);
-					break;
-				case "ls":
-					// Todo ls function
-					// ls();
-					break;
-				case "mkdir":
-					// Todo mkdir function
-					// mkdir(consoleInput[1]);
-					break;
-				case "delete":
-					// Todo delete function
-					// delete(consoleInput[1]);
-					break;
 				case "upload":
-					// Todo upload function
-					// upload(consoleInput[1]);
+					// Todo Download function
+					upload(consoleInput[1] , out);
 					break;
 				case "download":
-					Boolean zip = false;
-					if(consoleInput[2] =="-Z"){
-						zip = true;
-					}
+
 					// Todo Download function
-					// download(consoleInput[1], zip);
+					boolean isArchived = false;
+					if(consoleInput.length == 3){
+						isArchived = true;
+					}
+					download(consoleInput[1],isArchived, in);
+					break;
+				case "exit":
+					System.exit(0);
 					break;
 				default:
 					break;
@@ -116,5 +107,48 @@ public class Client {
 			return false;
 		}
 		return false;
+	}
+
+	public static void upload(String nomFichier, DataOutputStream out ) throws Exception{
+
+		String currentRep = System.getProperty("user.dir");
+		File  newFile = new File(currentRep + "/" + nomFichier);
+
+		InputStream uploadedFile = new FileInputStream(newFile);
+		int packetSize;
+
+		int fileLenght = (int) newFile.length();
+		out.write(fileLenght);
+
+		byte [] buffer = new byte[fileLenght];
+		while((packetSize = uploadedFile.read(buffer)) != -1){
+			out.write(buffer, 0, packetSize);
+
+		}
+		uploadedFile.close();
+	}
+
+	public static void download(String nomFichier, boolean isArchived, DataInputStream in) throws Exception{
+
+		String currentRep = System.getProperty("user.dir");
+
+		if(isArchived){
+			nomFichier = nomFichier.substring(0, nomFichier.indexOf(".")) + ".zip";
+		}
+		System.out.println("nom Fichier " + nomFichier);
+		File  newFile = new File(currentRep + "/" + nomFichier);
+		FileOutputStream fileOut = new FileOutputStream(newFile);
+
+		if(isArchived) {
+			FileOutputStream zipFile = new FileOutputStream(currentRep + "/" + nomFichier);
+			ZipOutputStream zipFileOutput = new ZipOutputStream(zipFile);
+			zipFileOutput.putNextEntry(new ZipEntry(newFile.getName())); // copies les byte de ran.jpeg dans le fichier zip
+		}
+		byte[] buffer = new byte[in.readInt()];
+		int sizeOfPacket;
+		while ((sizeOfPacket = in.read(buffer)) != -1) {  // store packet de donne dans le fichier
+			fileOut.write(buffer, 0, sizeOfPacket);  // store les donne contenue dans le buffer et le met dans le fichier "newFile" cree
+		}
+		fileOut.close();
 	}
 }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,13 +1,14 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Server {
 
@@ -25,6 +26,7 @@ public class Server {
 
         // Input et validation de l'adresse IP
         System.out.println(" Enter the server's IP address:");
+        System.out.println(System.getProperty("user.dir"));
 
         serverAdress = inputScanner.nextLine();
         while (!Server.validIPAddress(serverAdress)) {
@@ -67,7 +69,7 @@ public class Server {
             if (InetAddress.getByName(IP) instanceof InetAddress) {
                 return true;
             }
-		} catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
         return false;
@@ -78,65 +80,72 @@ public class Server {
 class ClientHandler extends Thread {
 
     private final Socket socket;
+    String serverAdress = "127.0.0.1";
+    int serverPort = 5002;
     private int clientNumber;
-	String serverAdress = "127.0.0.1";
-	int serverPort = 5002;
+    private String currentRep = System.getProperty("user.dir");
 
     public ClientHandler(Socket socket, int clientNumber, String serverAdress, int serverPort) {
+
         this.socket = socket;
         this.clientNumber = clientNumber;
-		this.serverAdress = serverAdress;
-		this.serverPort = serverPort;
+        this.serverAdress = serverAdress;
+        this.serverPort = serverPort;
         System.out.println("New connection with client#" + clientNumber + " at " + socket);
 
     }
 
-    public void commandCenter(DataInputStream in, DataOutputStream out) throws IOException {
+    public void commandCenter(DataInputStream in, DataOutputStream out) throws Exception {
         // Date et temps pour l'affichage
         LocalDate date = java.time.LocalDate.now();
         int hours = java.time.LocalTime.now().getHour(), minutes = java.time.LocalTime.now().getMinute(),
                 seconds = java.time.LocalTime.now().getSecond();
 
         while (true) {
-			String[] clientInputs = new String[] {};
-			String input = "";
-			try {
-				input = in.readUTF();
-				clientInputs = input.split(" ");
-				System.out.println("[" + this.serverAdress + ":" + this.serverPort + " - " + date + "@" + hours + ":" + minutes + ":" + seconds + "] : " + input);
-			} catch (Exception e) {}
+            String[] clientInputs = new String[]{};
+            String input = "";
+            try {
+                input = in.readUTF();
+                clientInputs = input.split(" ");
+                System.out.println("[" + this.serverAdress + ":" + this.serverPort + " - " + date + "@" + hours + ":" + minutes + ":" + seconds + "] : " + input);
+            } catch (Exception e) {
+            }
             switch (clientInputs[0]) {
                 case "cd":
-                    // Todo cd function
-                    // cd(clientInputs[1], out);
+                    if (clientInputs.length == 1) {
+                        out.writeUTF("Veuillez entree un nom de fichier");
+                    } else {
+                        cd(clientInputs[1], out);
+                    }
                     break;
                 case "ls":
-                    // Todo ls function
-                    // ls(out);
+                    ls(out, false);
                     break;
                 case "mkdir":
-                    // Todo mkdir function
-                    // mkdir(clientInputs[1], out);
+                    mkdir(clientInputs[1], out);
+
                     break;
                 case "delete":
-                    // Todo delete function
-                    // delete(clientInputs[1], out);
+                    delete(clientInputs[1], out);
+
                     break;
                 case "upload":
-                    // Todo upload function
-                    // upload(clientInputs[1], out);
+                    upload(clientInputs[1], in);
                     break;
                 case "download":
+                    if (clientInputs.length == 3 && clientInputs[2].contains("-z")) {
+                        Zip(clientInputs[1], out);
+                    } else {
 
-                    // Todo Download function
-                    // download(clientInputs[1], clientInputs[2], out);
+                        download(clientInputs[1], out);
+                   }
                     break;
-				default:
-					out.writeUTF("Command not found, please try again.\n");
-					break;
+                default:
+                    out.writeUTF("Command not found, please try again.\n");
+                    break;
             }
-			input = "";
-			clientInputs = new String[] {};
+            input = "";
+            clientInputs = new String[]{};
         }
 
 
@@ -150,7 +159,7 @@ class ClientHandler extends Thread {
             commandCenter(in, out);
             clientNumber++;
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("Error handling client#" + clientNumber + " : " + e);
 
         } finally {
@@ -163,4 +172,129 @@ class ClientHandler extends Thread {
             System.out.println("Connection with client#" + clientNumber + "closed");
         }
     }
+
+    public void cd(String name, DataOutputStream out) throws Exception {
+        if (name.startsWith("..")) {
+            String[] folderBackward = name.split("/");
+            String[] splitPath = currentRep.split("/");
+            String newPath = "";
+
+            int nBack = splitPath.length - folderBackward.length;
+            for (int i = 0; i < nBack; i++) {
+                newPath += "/" + splitPath[i];
+            }
+
+            currentRep = newPath;
+        } else if (ls(out, true).contains(name)) {
+
+            currentRep += "/" + name;
+        } else {
+            out.writeUTF(name + "ce dossier n'est pas present dans le Dossier parent");
+            return;
+        }
+        out.writeUTF("Vous êtes dans le dossier " + name);
+    }
+
+    public List<String> ls(DataOutputStream out, boolean isLookout) throws Exception {
+        File newRep = new File(currentRep);
+        File[] list = newRep.listFiles();
+        List<String> lsList = new ArrayList<String>();
+
+        for (int i = 0; i < list.length; i++) {
+            if (isLookout) {
+                if (list[i].isDirectory())
+                    lsList.add(list[i].getName());
+            } else {
+                if (list[i].isDirectory()) {
+                    out.writeUTF("[Folder] " + list[i].getName());
+
+                } else {
+                    out.writeUTF("[File] " + list[i].getName());
+
+                }
+            }
+        }
+        return lsList;
+    }
+
+    public void mkdir(String name, DataOutputStream out) throws Exception {
+
+        File newFile = new File(currentRep + "/" + name);
+        if (newFile.mkdir()) {
+            out.writeUTF("Le dossier " + name + " a ete cree");
+        } else {
+            out.writeUTF("Error: aucun dossier n'as ete cree. Essayer encore ");
+        }
+    }
+
+    public void delete(String name, DataOutputStream out) throws Exception {
+
+        if (!ls(out, true).contains(name)) {
+            out.writeUTF("Aucun dossier ou fichier de ce nom existe");
+            return;
+        }
+        File newRep = new File(currentRep + "/" + name);
+        newRep.delete();
+        out.writeUTF("Le dossier " + name + " a été supprime.");
+    }
+
+    public void upload(String nomFichier, DataInputStream in) throws Exception {
+
+
+        System.out.println(currentRep + "/" + nomFichier);
+
+        File newFile = new File(currentRep + "/" + nomFichier);
+        FileOutputStream fileOut = new FileOutputStream(newFile);
+        byte[] buffer = new byte[in.read()];
+        int sizeOfPacket;
+        while ((sizeOfPacket = in.read(buffer)) != -1) { // store packet de donne dans le fichier
+            fileOut.write(buffer, 0, sizeOfPacket); // store les donne contenue dans le buffer et le met dans le fichier "newFile" cree
+        }
+        fileOut.close();
+    }
+
+    public void download(String nomFichier, DataOutputStream out) throws Exception {
+
+        File newFile = new File(currentRep + "/" + nomFichier);
+        InputStream uploadedFile = new FileInputStream(newFile);
+
+        int packetSize;
+        int fileLenght = (int) newFile.length();
+        System.out.println(fileLenght);
+        out.writeInt(fileLenght);
+        byte [] buffer = new byte[fileLenght];
+
+        while ((packetSize = uploadedFile.read(buffer)) != -1) {
+            out.write(buffer, 0, packetSize);
+        }
+        uploadedFile.close();
+    }
+
+    public void Zip(String nomFichier, DataOutputStream out) throws Exception {
+
+        File newFile = new File(currentRep + "/" + nomFichier);
+        String zipFileName = nomFichier.substring(0, nomFichier.indexOf(".")) + ".zip";
+
+
+        FileOutputStream zipFile = new FileOutputStream(currentRep + "/" + zipFileName);
+        ZipOutputStream zipFileOutput = new ZipOutputStream(zipFile);
+        zipFileOutput.putNextEntry(new ZipEntry(newFile.getName())); // copies les byte de ran.jpeg dans le fichier zip
+
+        InputStream fileToZip = new FileInputStream(newFile);
+
+
+        byte [] buffer = new byte[(int) newFile.length()];
+        int packetSize;
+        while ((packetSize = fileToZip.read(buffer)) != -1) {
+            zipFileOutput.write(buffer, 0, packetSize);
+
+        }
+
+        zipFileOutput.closeEntry();
+        zipFileOutput.close();
+        fileToZip.close();
+
+        download(zipFileName, out);
+    }
+
 }
